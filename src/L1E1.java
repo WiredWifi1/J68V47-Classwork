f29d1aa21b707ec87c83b2dd65c6f6583d9d5873
30130302 <30130302@nescol.ac.uk>
2023-08-30 15:44:45 +0100

    .

---
+++ src/L1E1.java
@@ -0,0 +1,9 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class L1E1 {
+    public static void main(String[] args) {
+        System.out.println("War and violence spread");
+        System.out.println("Innocent lives lost each day");
+        System.out.println("When will peace prevail?");
+    }
+}
